f421bab84e0ca70542ee010d0da98d03d99f4368
c3zhku00 <c3zhku00@students.oamk.fi>
2024-02-28 22:16:35 +0200

    ScaffoldApp

---
+++ ScaffoldApp/app/src/main/java/com/example/scaffoldapp/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.scaffoldapp
+
+import android.icu.text.IDNA.Info
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.scaffoldapp.ui.theme.ScaffoldAppTheme
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlin.math.exp
+import com.example.scaffoldapp.ui.theme.ScaffoldAppTheme
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScaffoldAppTheme {
+                ScaffoldApp()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ) {
+        composable(route = "Home") {
+            MainScreen(navController)
+        }
+        composable(route = "Info") {
+            InfoScreen(navController)
+        }
+        composable(route = "Settings") {
+            SettingsScreen(navController)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(title: String, navController: NavController) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    TopAppBar(
+        title = { Text(title) },
+        actions = {
+            IconButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = null)
+            }
+            DropdownMenu(expanded = expanded,
+                onDismissRequest = { expanded = false }) {
+                DropdownMenuItem(text = { Text(text = "Info") }, onClick = { navController.navigate("info") })
+                DropdownMenuItem(text = { Text(text = "Settings") }, onClick = { navController.navigate("settings") })
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenTopBar(title: String, navController: NavController) {
+    TopAppBar(title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar("My App", navController) },
+        content = { paddingValues ->
+            Text(
+                text = "Content for Home screen",
+                modifier = Modifier.padding(paddingValues)
+            )
+        },
+    )
+}
+
+@Composable
+fun InfoScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar("Info", navController) },
+        content = { paddingValues ->
+            Text(
+                text = "Content for Info screen",
+                modifier = Modifier.padding(paddingValues)
+            )
+        },
+    )
+}
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar("Settings", navController) },
+        content = { paddingValues ->
+            Text(
+                text = "Content for Settings screen",
+                modifier = Modifier.padding(paddingValues)
+            )
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScaffoldDefaultsPreview() {
+    ScaffoldAppTheme {
+        ScaffoldApp()
+    }
+}
